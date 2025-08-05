fd536f3ee548593bb3b525e8a2cbd3dbf9dceb57
abhishek devop <standup787059@gmail.com>
2025-08-06 00:04:13 +0530

    uploaded songs on cloud

--- backend/src/service/storage.service.js
+++ backend/src/service/storage.service.js
@@ -1,5 +1,5 @@
 var ImageKit = require("imagekit");
-
+var mongoose = require('mongoose')
 var imagekit = new ImageKit({
   publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
   privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
@@ -11,7 +11,8 @@ function uploadFile(file) {
     imagekit.upload(
       {
         file: file.buffer,
-        fileName: file.originalname,
+        fileName: new mongoose.Types.ObjectId().toString(),
+        folder: "MoodyPlayer-audio",
       },
       (error, result) => {
         if (error) {
